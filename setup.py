282cfb5b29c0fbb120d2334a6dabdc83b8ae604b
bean980310 <bean980310@hotmail.co.kr>
2024-10-28 20:40:13 +0900

    Bug fixed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="civitai-downloader",
-    version="0.2.0",
+    version="0.2.1",
     author="Hyunbeen Chang",
     author_email="bean980310@gmail.com",
     description="A package to download models from CivitAI",
