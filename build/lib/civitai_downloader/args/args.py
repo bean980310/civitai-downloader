fceb05cad32df8afd954da3679f3de30b4fd5ddb
bean980310 <bean980310@hotmail.co.kr>
2024-10-30 14:25:40 +0900

    Apply .gitignore

--- build/lib/civitai_downloader/args/args.py
+++
@@ -1,9 +0,0 @@
-import argparse
-    
-def register():
-    parser=argparse.ArgumentParser(description='Download a model from CivitAI')
-    parser.add_argument('model_id', type=int, help='CivitAI Model ID, eg: 46846')
-    parser.add_argument('local_dir', type=str, help='Output path, eg: /workspace/stable-diffusion-webui/models/Stable-diffusion', default='./')
-    parser.add_argument('--version', action='version', version='%(prog)s 0.2.1', help='Show the version number and exit')
-
-    return parser.parse_args()
