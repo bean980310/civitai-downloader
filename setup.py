8d3f5f3840541dbbfb62656c6beb37ed056aeb4c
bean980310 <bean980310@hotmail.co.kr>
2024-10-30 12:01:21 +0900

    Build Update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="civitai-downloader",
-    version="0.3.1",
+    version="0.3.2",
     author="Hyunbeen Chang",
     author_email="bean980310@gmail.com",
     description="A package to download models from CivitAI",
