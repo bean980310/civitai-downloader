10c7d7bdc0c7f0a0a8fecbf3ef68b4a82bda1435
bean980310 <bean980310@hotmail.co.kr>
2024-10-31 20:42:32 +0900

    Add Download Link on API Used Code

--- src/civitai_downloader/api/model.py
+++ src/civitai_downloader/api/model.py
@@ -29,17 +29,20 @@ def get_model_info_from_api(
         model_creator_name=data.get('creator').get('username')
         model_creator_image=data.get('creator').get('image')
         model_tags=[data.get('tags')[i] for i in range(len(data.get('tags')))]
-        model_version=[{
-            'id': data.get('modelVersions')[i].get('id'),
-            'name': data.get('modelVersions')[i].get('name'),
-            'createdAt': data.get('modelVersions')[i].get('createdAt'),
-            'updatedAt': data.get('modelVersions')[i].get('updatedAt'),
-            'trainedWords': [data.get('modelVersions')[i].get('trainedWords')[j] for j in range(len(data.get('modelVersions')[i].get('trainedWords')))],
-            'baseModel': data.get('modelVersions')[i].get('baseModel'),
-            'description': data.get('modelVersions')[i].get('description') if include_desc else '',
-            'downloadUrl': data.get('modelVersions')[i].get('downloadUrl')
-        } for i in range(len(data.get('modelVersions')))]
-        return model_id, model_name, model_desc, model_type, model_poi, model_is_nsfw, model_allow_no_credit, model_allow_commercial_use, model_stats, model_creator_name, model_creator_image, model_tags, model_version
+        model_version=data.get('modelVersions', {})
+        model_version_info=[{
+            'id': model_version[i].get('id'),
+            'name': model_version[i].get('name'),
+            'createdAt': model_version[i].get('createdAt'),
+            'updatedAt': model_version[i].get('updatedAt'),
+            'trainedWords': model_version[i].get('trainedWords'),
+            'baseModel': model_version[i].get('baseModel'),
+            'description': model_version[i].get('description'),
+            'downloadUrl': model_version[i].get('downloadUrl'),
+            'files.name': [model_version[i].get('files')[j].get('name') for j in range(len(model_version[i].get('files')))],
+            'images.url': [model_version[i].get('images')[j].get('url') for j in range(len(model_version[i].get('images')))]
+        } for i in range(len(model_version))]
+        return model_id, model_name, model_desc, model_type, model_poi, model_is_nsfw, model_allow_no_credit, model_allow_commercial_use, model_stats, model_creator_name, model_creator_image, model_tags, model_version_info
     else:
         error_code=f'{response.status_code} : {response.text}'
         return error_code
@@ -47,7 +50,6 @@ def get_model_info_from_api(
 def get_model_version_info_from_api(
         model_version_id: int,
         api_token: Optional[str]=None, 
-        include_desc: Optional[bool]=False
 ):
     api_url=f'{CIVITAI_API_URL}/model-versions/{model_version_id}'
     headers={'Authorization': f'Bearer {api_token}'} if api_token is not None else {}
@@ -59,8 +61,11 @@ def get_model_version_info_from_api(
         model_created=data.get('createdAt')
         model_updated=data.get('updatedAt')
         model_trained_words=[data.get('trainedWords')[i] for i in range(len(data.get('trainedWords')))]
-        model_version_desc=data.get('description') if include_desc else None
+        model_version_desc=data.get('description')
         base_model=data.get('baseModel')
+        model_version_files_name=[data.get('files')[i].get('name') for i in range(len(data.get('files')))]
+        model_version_files_url=[data.get('files')[i].get('url') for i in range(len(data.get('files')))]
+        model_version_images_url=[data.get('images')[i].get('url') for i in range(len(data.get('images')))]
         return model_version_id, model_id, model_version_name, model_created, model_updated, model_trained_words, base_model, model_version_desc
     else:
         error_code=f'{response.status_code} : {response.text}'
